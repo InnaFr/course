0485db9de5fb955ac452d4d7f8298e28ffd416df
InnaFr <fr.monstr@gmail.com>
2018-06-03 12:59:50 +0800

    Дз_3

---
+++ hw_3/WindowsFormsApplication2/Form1.Designer.cs
@@ -0,0 +1,244 @@
+﻿namespace WindowsFormsApplication2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(34, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(444, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Укажите имена игроков или используйте значения по умолчанию";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(102, 50);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Visible = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(37, 190);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 27);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Применить";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Visible = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(34, 100);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(372, 17);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Выберите символ, которым будет играть первый игрок";
+            this.label2.Visible = false;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(37, 141);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(41, 21);
+            this.checkBox1.TabIndex = 5;
+            this.checkBox1.Text = "О";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.Visible = false;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(98, 141);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(39, 21);
+            this.checkBox2.TabIndex = 6;
+            this.checkBox2.Text = "X";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            this.checkBox2.Visible = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(34, 50);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(46, 17);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "label3";
+            this.label3.Visible = false;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(37, 75);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 22);
+            this.textBox2.TabIndex = 8;
+            this.textBox2.Text = "Игрок 1";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(234, 75);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 22);
+            this.textBox3.TabIndex = 9;
+            this.textBox3.Text = "Игрок 2";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(37, 112);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 10;
+            this.button2.Text = "Далее";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(76, 305);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(174, 23);
+            this.button3.TabIndex = 11;
+            this.button3.Text = "Закончить играть";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Visible = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(304, 305);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(174, 23);
+            this.button4.TabIndex = 12;
+            this.button4.Text = "Сыграть снова";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Visible = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(34, 33);
+            this.label4.MaximumSize = new System.Drawing.Size(550, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(527, 17);
+            this.label4.TabIndex = 14;
+            this.label4.Text = "Имя игрока | Раунд текущей игры | Статус игрока | Количество шагов за раунд\r\n";
+            this.label4.Visible = false;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(198, 193);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(280, 23);
+            this.button5.TabIndex = 15;
+            this.button5.Text = "Ознакомиться с результатами";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Visible = false;
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(387, 164);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(192, 23);
+            this.button6.TabIndex = 16;
+            this.button6.Text = "Удалить данные из базы";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Visible = false;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(604, 412);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+    }
+}
+
---
+++ hw_3/WindowsFormsApplication2/Form1.cs
@@ -0,0 +1,487 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+namespace WindowsFormsApplication2
+{
+    interface Inform
+    {
+        void StatusGame();
+        void Info();
+        void menu();
+    }
+
+    public partial class Form1 : Form, Inform
+    {
+        public int Round = 0;
+        public bool WhoFirst = false;
+        public int NumberGamer = 0;
+        public bool thereIsWinner = false;
+        public int N;
+        public string gamer_1_Name = "";
+        public string gamer_2_Name = "";
+
+        public TableLayoutPanel table = new TableLayoutPanel
+        {
+            AutoSize = true
+        };
+        public void menu()
+        {
+            gamer_1_Name = textBox2.Text;
+            gamer_2_Name = textBox3.Text;
+            if (gamer_1_Name != "" && gamer_2_Name != "")
+            {
+                textBox2.Visible = false;
+                textBox3.Visible = false;
+                button2.Visible = false;
+                button3.Visible = false;
+                button4.Visible = false;
+                textBox1.Visible = true;
+                label2.Visible = true;
+                checkBox1.Visible = true;
+                checkBox2.Visible = true;
+                button1.Visible = true;
+            }
+            else
+            {
+                MessageBox.Show("Введите имена игроков!");
+            }
+
+        }
+        public void StatusGame()
+        {
+            if (NumberGamer % 2 == 1)
+            {
+                label3.Text = "Ожидается ход игрока " + gamer_2_Name;
+            }
+            else
+            {
+                label3.Text = "Ожидается ход игрока " + gamer_1_Name;
+            }
+            NumberGamer++;
+        }
+
+        public void Info()
+        {
+            if (NumberGamer == 0)
+            {
+                label1.Text = "Игра началась";
+            }
+            else if (thereIsWinner == true)
+            {
+                if (NumberGamer % 2 == 0)
+                {
+                    label1.Text = "Победил игрок " + gamer_1_Name;
+                    WorkWithBD(gamer_1_Name, gamer_2_Name, NumberGamer - 1, "true");
+                    NumberGamer = 0;
+                    thereIsWinner = false;
+                    button3.Visible = true;
+                    button4.Visible = true;
+                }
+                else
+                {
+                    label1.Text = "Победил игрок " + gamer_2_Name;
+                    WorkWithBD(gamer_1_Name, gamer_2_Name, NumberGamer - 1, "false");
+                    NumberGamer = 0;
+                    thereIsWinner = false;
+                    button3.Visible = true;
+                    button4.Visible = true;
+                }
+            }
+            else if (table.Enabled == false)
+            {
+                label1.Text = "Ничья";
+
+                WorkWithBD(gamer_1_Name, gamer_2_Name, NumberGamer - 1, "-1");
+                NumberGamer = 0;
+            }
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public void PrintResults()
+        {
+            using (SqlConnection cn = new SqlConnection("Data Source=LAPTOP-8FE5V0OM\\SQLEXPRESS;Initial Catalog=PlayGame;Integrated Security=True"))
+            {
+                cn.Open();
+
+                label4.Text += "\n";
+                label4.Visible = true;
+                button5.Visible = false;
+                string query = "SELECT * FROM Game";
+                SqlCommand command = new SqlCommand(query, cn);
+                IDataReader reader = command.ExecuteReader();
+                while (reader.Read())
+                {
+                    for (int i = 0; i < reader.FieldCount; i++)
+                    {
+                        string p = "";
+                        if (reader[i].ToString() == "true")
+                        {
+                            p = " | победа";
+                        }
+                        else if (reader[i].ToString() == "false")
+                        {
+                            p = " | поражение";
+                        }
+                        else if (reader[i].ToString()=="-1")
+                        {
+                            p = " | ничья";
+                        }
+                        else
+                        {
+                            p = reader[i].ToString();
+                        }
+                        label4.Text += " |  " + p;
+                    }
+                    label4.Text += "\n";
+                }
+            }
+        }
+        public void WorkWithBD(string nameFirst,
+                               string nameSecond,
+                               int numStepFirst,
+                               string winF="-1")
+        {
+            Round++;
+            using (SqlConnection cn = new SqlConnection("Data Source=LAPTOP-8FE5V0OM\\SQLEXPRESS;Initial Catalog=PlayGame;Integrated Security=True"))
+            {
+
+                cn.Open();
+                if (winF == "true")
+                {
+                    string queryFirst = "INSERT INTO Game (NameGamer, Round, WinORLose, NumberStroke) Values ('" + nameFirst + "'," + Round + ",'true'," + numStepFirst + ")";
+                    string querySecond = "INSERT INTO Game (NameGamer, Round, WinORLose, NumberStroke) Values ('" + nameSecond + "'," + Round + ",'false'," + (numStepFirst - 1) + ")";
+                    SqlCommand first = new SqlCommand(queryFirst, cn);
+                    SqlCommand second = new SqlCommand(querySecond, cn);
+                    first.ExecuteNonQuery();
+                    second.ExecuteNonQuery();
+                }
+                else if (winF == "false")
+                {
+                    string queryFirst = "INSERT INTO Game (NameGamer, Round, WinORLose, NumberStroke) Values ('" + nameFirst + "'," + Round + ",'false'," + (numStepFirst - 1) + ")";
+                    string querySecond = "INSERT INTO Game (NameGamer, Round, WinORLose, NumberStroke) Values ('" + nameSecond + "'," + Round + ",'true'," + numStepFirst + ")";
+                    SqlCommand first = new SqlCommand(queryFirst, cn);
+                    SqlCommand second = new SqlCommand(querySecond, cn);
+                    second.ExecuteNonQuery();
+                    first.ExecuteNonQuery();
+                }
+
+                else
+                {
+                    string queryFirst = "INSERT INTO Game (NameGamer, Round, WinORLose, NumberStroke) Values ('" + nameFirst + "'," + Round + ",'-1'," + numStepFirst + ")";
+                    string querySecond = "INSERT INTO Game (NameGamer, Round, WinORLose, NumberStroke) Values ('" + nameSecond + "'," + Round + ",'-1'," + (numStepFirst - 1) + ")";
+                    SqlCommand first = new SqlCommand(queryFirst, cn);
+                    SqlCommand second = new SqlCommand(querySecond, cn);
+                    first.ExecuteNonQuery();
+                    second.ExecuteNonQuery();
+                }
+            }
+        }
+        public void MyButtonClick(object sender, EventArgs e)
+        {
+            if (((Button)sender).Text == "?")
+            {
+                StatusGame();
+                if (WhoFirst == true)
+                {
+                    if (NumberGamer % 2 == 1)
+                    {
+                        ((Button)sender).Text = "X";
+                    }
+                    else
+                    {
+                        ((Button)sender).Text = "O";
+                    }
+                }
+                else
+                {
+                    if (NumberGamer % 2 == 1)
+                    {
+                        ((Button)sender).Text = "O";
+                    }
+                    else
+                    {
+                        ((Button)sender).Text = "X";
+                    }
+                }
+                Winner();
+                Info();
+            }
+            else
+            {
+                MessageBox.Show("Ячейка занята!");
+            }
+        }
+
+        public void Winner()
+        {
+            int ii = 0;
+            int jj = 0;
+            int schet_1 = 0;
+            int schet_2 = 0;
+            Button[,] btn = new Button[N, N];
+            foreach (Button item in table.Controls)
+            {
+                if (ii < N)
+                {
+                    btn[ii, jj] = item;
+                    ii++;
+                }
+                else if (ii == N)
+                {
+                    jj++;
+                    ii = 0;
+                    btn[ii, jj] = item;
+                    ii++;
+                }
+            }
+
+            for (int i = 0; i < N; i++)
+            {
+                if (btn[i, i].Text != btn[1, 1].Text || btn[i, i].Text == "?")
+                {
+                    schet_1++;
+                }
+
+                if (btn[i, N - i - 1].Text != btn[0, N - 1].Text || btn[i, N - i - 1].Text == "?")
+                {
+                    schet_2++;
+                }
+            }
+
+            if (schet_1 == 0 || schet_2 == 0)
+            {
+                MessageBox.Show("Игра окончена");
+                label3.Visible = false;
+                thereIsWinner = true;
+                if (schet_1 == 0)
+                {
+                    for (int i = 0; i < N; i++)
+                    {
+                        table.Controls[i * N + i].BackColor = Color.FromKnownColor(KnownColor.Coral);
+                    }
+                }
+                if (schet_2 == 0)
+                {
+                    for (int i = 0; i < N; i++)
+                    {
+                        table.Controls[i * N + N - i - 1].BackColor = Color.FromKnownColor(KnownColor.Coral);
+                    }
+                }
+                table.Enabled = false;
+            }
+
+
+            for (int i = 0; i < N; i++)
+            {
+                schet_1 = 0;
+                schet_2 = 0;
+
+                for (int j = 0; j < N; j++)
+                {
+                    if (btn[i, 0].Text != btn[i, j].Text || btn[i, j].Text == "?")
+                    {
+                        schet_1++;
+                    }
+
+                    if (btn[0, i].Text != btn[j, i].Text || btn[j, i].Text == "?")
+                    {
+                        schet_2++;
+                    }
+                }
+
+                if (schet_1 == 0 || schet_2 == 0)
+                {
+                    MessageBox.Show("Игра окончена");
+                    label3.Visible = false;
+                    thereIsWinner = true;
+                    if (schet_1 == 0)
+                    {
+                        for (int j = 0; j < N; j++)
+                        {
+                            table.Controls[j * N + i].BackColor = Color.FromKnownColor(KnownColor.Coral);
+                        }
+                    }
+                    if (schet_2 == 0)
+                    {
+                        for (int j = 0; j < N; j++)
+                        {
+                            table.Controls[i * N + j].BackColor = Color.FromKnownColor(KnownColor.Coral);
+                        }
+                    }
+                    table.Enabled = false;
+                }
+            }
+            NoWinner(btn);
+        }
+
+        public void NoWinner(Button[,] btn)
+        {
+            int k = 0;
+            for (int i = 0; i < N; i++)
+            {
+                for (int j = 0; j < N; j++)
+                {
+                    if (btn[i, j].Text == "?")
+                    {
+                        k++;
+                    }
+                }
+            }
+            if (k == 0 && thereIsWinner == false)
+            {
+                table.Enabled = false;
+                button3.Visible = true;
+                button4.Visible = true;
+                label3.Visible = false;
+                MessageBox.Show("Ничья");
+            }
+        }
+        public void InputNumber()
+        {
+            if (textBox1.Text != "")
+            {
+                try
+                {
+                    N = int.Parse(textBox1.Text);
+                }
+                catch (FormatException e_1)
+                {
+                    MessageBox.Show("Ошибка: " + e_1.Message);
+                    textBox1.Text = "";
+                }
+            }
+            else
+            {
+                MessageBox.Show("Введите числовое значение в поле ввода!");
+            }
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            InputNumber();
+            table.Visible = true;
+            if (checkBox1.Checked && checkBox2.Checked)
+            {
+                MessageBox.Show("Необходимо выбрать один вариант!");
+                checkBox1.Checked = false;
+                checkBox2.Checked = false;
+            }
+
+            if ((checkBox1.Checked || checkBox2.Checked) && textBox1.Text != "")
+            {
+                if (checkBox1.Checked)
+                {
+                    WhoFirst = true;
+                }
+                else
+                {
+                    WhoFirst = false;
+                }
+                table.Enabled = true;
+                table.ColumnCount = N;
+                table.RowCount = N;
+                Controls.Add(table);
+                table.Location = new Point(60, 60);
+                Button[,] bt = new Button[N, N];
+                for (int i = 0; i < N; i++)
+                {
+                    for (int j = 0; j < N; j++)
+                    {
+                        bt[i, j] = new Button();
+                        bt[i, j].Text = "?";
+                        bt[i, j].Click += MyButtonClick;
+                        bt[i, j].Width = 30;
+                        bt[i, j].Height = 30;
+                        table.Controls.Add(bt[i, j]);
+                    }
+                }
+                Info();
+                label3.Visible = true;
+                StatusGame();
+                textBox1.Visible = false;
+                button1.Visible = false;
+                checkBox1.Visible = false;
+                checkBox2.Visible = false;
+                label2.Visible = false;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            label1.Text = "Введите размерность игрового поля";
+            menu();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            label1.Text = "Укажите имена игроков или используйте значения по умолчанию";
+            label3.Visible = false;
+            textBox1.Visible = false;
+            label2.Visible = false;
+            checkBox1.Visible = false;
+            checkBox2.Visible = false;
+            button3.Visible = false;
+            button4.Visible = false;
+            textBox2.Visible = true;
+            textBox3.Visible = true;
+            button2.Visible = true;
+            table.Visible = false;
+            for (int i = 0; i <= N; i++)
+            {
+                foreach (Button item in table.Controls)
+                {
+                    Controls.Remove(item);
+                    item.Dispose();
+                }
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            label1.Visible = false;
+            label2.Visible = false;
+            label3.Visible = false;
+            label4.Visible = false;
+            button1.Visible = false;
+            button2.Visible = false;
+            button3.Visible = false;
+            button4.Visible = false;
+            table.Visible = false;
+            button5.Visible = true;
+            button5.Click += new EventHandler(button5_Click);
+            Controls.Add(button5);
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            button5.Visible = false;
+            button6.Visible = true;
+            label4.Visible = true;
+            PrintResults();
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            using (SqlConnection cn = new SqlConnection("Data Source=LAPTOP-8FE5V0OM\\SQLEXPRESS; Initial Catalog = XorO; Integrated Security = True"))
+            {
+                cn.Open();
+                string query = "DELETE FROM Game";
+                SqlCommand doIt = new SqlCommand(query, cn);
+                doIt.ExecuteNonQuery();
+            }
+            label4.Text = "...";
+        }
+    }
+}
+
